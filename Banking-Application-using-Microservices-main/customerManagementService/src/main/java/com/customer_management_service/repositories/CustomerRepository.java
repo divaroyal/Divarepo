e921baac651fb4d468140a948aff57091511e209
divaroyal <divakar.sodanapalli@gmail.com>
2023-08-14 18:57:28 +0530

    Delete Banking-Application-using-Microservices-main directory

--- Banking-Application-using-Microservices-main/customerManagementService/src/main/java/com/customer_management_service/repositories/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.customer_management_service.repositories;
-
-import com.customer_management_service.entites.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, String> {
-
-
-}
